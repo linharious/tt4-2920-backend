7bebe91ee1ea7ed3d4f01396d6dfefbfb0294cff
linharious <linh.t.tn14@gmail.com>
2026-03-26 12:52:50 -0400

    auth update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,12 @@
 const user = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+const generateToken = () => {
+  const jwtSecret = "secret3001";
+
+  jwt.sign({ id, email }, jwtSecret, { expiresIn: "1d" });
+};
 const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -53,4 +59,49 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "email, password are required",
+      //   code: 400,
+    });
+  }
+
+  const user = await user.findOne({
+    email: String(email).toLowerCase(),
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "invalid email or password",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "invalid email or password",
+    });
+  }
+
+  const token = generateToken(String(user._id), user.email);
+  console.log(token);
+  return res.json({
+    message: "login sucess",
+    data: {
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        createAt: user.createAt,
+        updateAt: user.updateAt,
+      },
+    },
+  });
+};
+
+module.exports = { register, login };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,10 +2,12 @@
 // export the modules and import into the app.js
 
 const express = require("express");
-const { register } = require("../controllers/authController");
+const { register, login } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
+// not all language will allow to pass a body through post request
 
 module.exports = router;
